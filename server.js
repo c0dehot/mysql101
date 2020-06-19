12af5850e8fcd0e9238a08bbdb2fd0ae2d87d74a
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-19 13:12:32 -0400

    Added the dotenv to secure passwords

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// loads confirmation information from the .env file
+require('dotenv').config()
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
@@ -28,9 +31,9 @@ class Database {
 const db = new Database({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "r00tr00t",
-  database: "cms"
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME
 });
 
 async function mainApp(){
